5f26c26eacc379fc084bb8dcffef3a6321b11fe2
name <miia@com.tw>
2019-12-24 16:26:33 +0800

    MOVE

---
+++ app/src/main/java/com/dom/game/GameView.java
@@ -0,0 +1,28 @@
+package com.dom.game;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    int posX,posY =400;
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.warm);
+        canvas.drawBitmap(bitmap,posX,posY,paint);
+        Log.d("game",getWidth()+" "+getHeight());
+    }
+}
--- app/src/main/java/com/dom/game/MainActivity.java
+++ app/src/main/java/com/dom/game/MainActivity.java
@@ -9,10 +9,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+
+    private GameView gameview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findview();
 
     }
@@ -21,21 +25,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.arrowdown).setOnClickListener(this);
         findViewById(R.id.arrowleft).setOnClickListener(this);
         findViewById(R.id.arrowright).setOnClickListener(this);
+        gameview = findViewById(R.id.game);
     }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.arrowup:
                 Log.d("MainActivity","onClick:UP");
+                if(gameview.posY>150)
+                gameview.posY=gameview.posY-50;
+                gameview.invalidate();
                 break;
             case R.id.arrowdown:
                 Log.d("MainActivity","onClick:DOWN");
+                if(gameview.posY<1400)
+                gameview.posY=gameview.posY+50;
+                gameview.invalidate();
                 break;
             case R.id.arrowleft:
                 Log.d("MainActivity","onClick:LEFT");
+                if(gameview.posX>10)
+                    gameview.posX=gameview.posX-50;
+                gameview.invalidate();
                 break;
             case R.id.arrowright:
                 Log.d("MainActivity","onClick:RIGHT");
+                if(gameview.posX<1300)
+                    gameview.posX=gameview.posX+50;
+                gameview.invalidate();
                 break;
 
         }
